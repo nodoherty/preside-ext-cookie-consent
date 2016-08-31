aad8e2e45c4c3fbb5b8691acf9bb37183af0290f
Niall O'Doherty <niall.odoherty@pixl8.co.uk>
2016-08-31 21:34:29 +0100

    Removing redundant rename and clean dependencies

--- assets/gruntfile.js
+++ assets/gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function( grunt ) {
 
 	grunt.loadNpmTasks( "grunt-contrib-cssmin" );
 	grunt.loadNpmTasks( "grunt-contrib-less"   );
-	grunt.loadNpmTasks( "grunt-contrib-rename" );
 	grunt.loadNpmTasks( "grunt-contrib-watch"  );
 
 	grunt.registerTask( "default", [ "less", "cssmin" ] );
